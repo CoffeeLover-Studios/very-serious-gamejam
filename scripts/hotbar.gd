01496e42a7a220c96b91c0856d73ad14364753ae
CoffeeLover-Studios <coffeeloverstudios@gmail.com>
2026-06-22 17:28:45 +0100

    Updated Godot project

---
+++ scripts/hotbar.gd
@@ -0,0 +1,8 @@
+extends HBoxContainer
+
+@onready var slots: Array = get_children()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
